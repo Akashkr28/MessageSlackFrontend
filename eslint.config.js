fca3cf07b609fecc2c7b2e277b101cf504cddfe2
Akashkr28 <email.akash2010@gmail.com>
2024-12-14 05:32:30 +0530

    Added eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+
 
 export default [
   { ignores: ['dist'] },
@@ -20,10 +22,14 @@ export default [
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
+      'simple-import-sort': simpleImportSort,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      "react/prop-types": "off",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
@@ -33,6 +39,8 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"],
     },
   },
 ]
