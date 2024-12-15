734e91f2069f51506400091f0b5e6fdda7886347
Akashkr28 <email.akash2010@gmail.com>
2024-12-15 06:07:05 +0530

    Added signIn card

--- src/components/organisms/Auth/SignupCard.jsx
+++ src/components/organisms/Auth/SignupCard.jsx
@@ -3,8 +3,11 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const SignupCard = () => {
+
+    const navigate  = useNavigate();
 
     const [signupForm, setSignupForm] = useState({
         email: '',
@@ -65,7 +68,11 @@ export const SignupCard = () => {
                 <Separator className="my-4"/>
                 <p className="text-s text-muted-foreground mt-4">
                     Already have an account ? {' '}
-                    <span className="text-sky-600 hover:underline cursor-pointer">Sign In</span>
+                    <span 
+                        className="text-sky-600 hover:underline cursor-pointer"
+                        onClick={() => navigate('/auth/signin')}
+                    
+                    >Sign In</span>
                 </p>
              </CardContent>
         </Card>
