f026fd6f1b2f48ac49b0f4ffd495814516eddb87
Akashkr28 <email.akash2010@gmail.com>
2025-01-01 09:17:04 +0530

    Added new message sending events

--- src/components/molecules/ChatInput/ChatInput.jsx
+++ src/components/molecules/ChatInput/ChatInput.jsx
@@ -1,9 +1,32 @@
-import { Editor } from "@/components/atoms/Editor/Editor"
+import { Editor } from "@/components/atoms/Editor/Editor";
+import { useAuth } from "@/hooks/context/useAuth";
+import { useCurrentWorkspace } from "@/hooks/context/useCurrentWorkspace";
+import { useSocket } from "@/hooks/context/useSocket";
 
 export const ChatInput = () => {
 
+    const { socket, currentChannel } = useSocket();
+    const { auth } = useAuth();
+    const { currentWorkspace } = useCurrentWorkspace();
+
+
     async function handleSubmit ({ body }) {
+
+                if (!auth?.user?.id) {
+                    console.error('Sender ID (auth.user._id) is missing!');
+                    return;
+                }
+
+
         console.log(body);
+        socket?.emit('NewMessage', {
+            channelId: currentChannel,
+            body,
+            senderId: auth?.user?.id,
+            workspaceId: currentWorkspace?._id
+        }, (data) => {
+            console.log('Message sent', data);
+        });
     }
 
 
