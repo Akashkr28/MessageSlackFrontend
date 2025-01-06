ccedc84b41166dcd77270877423f5d55fe60cb79
Akashkr28 <email.akash2010@gmail.com>
2025-01-06 22:37:45 +0530

    Added BG

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,6 +1,6 @@
 export const Auth = ({ children }) => {
     return (
-        <div className="h-[100vh] w-[100vw] flex justify-center">
+        <div className="h-[100vh] w-[100vw] flex justify-center bg-[#BBDDF8]">
             <div className="h-auto w-[100vw]">
                 {children}
             </div>    
