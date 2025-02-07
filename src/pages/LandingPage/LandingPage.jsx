10d0c912e1684a20623abc322a0a4581fc31b22d
Akashkr28 <email.akash2010@gmail.com>
2025-02-07 22:20:29 +0530

    code fix

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,8 +2,6 @@ import { useNavigate } from "react-router-dom";
 
 import Banner from "@/components/atoms/Banner/Banner";
 import Navbar from "@/components/atoms/Navbar/Navbar";
-import StickyScrollReveal from "@/components/atoms/StickyScroll/StickyScroll";
-import { ContainerScroll } from "@/components/ui/container-scroll-animation";
 
 export const LandingPage = () => {
     const navigate = useNavigate();
